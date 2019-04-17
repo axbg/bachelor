fabaa702a50c93b4f104a1b84f71a6289e17b0bb
Alex Bisag <bisagalexstefan@gmail.com>
2019-04-17 08:13:46 +0300

    Created routes structure.

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,42 @@ router.get("/", async (ctx) => {
     ctx.body = { message: "flow - student module" };
 })
 
+router.get("/documents", async (ctx) => {
+    ctx.body = { message: "flow - student module - student get documents" };
+})
+
+router.get("/documents/:id", async (ctx) => {
+    ctx.body = { message: "flow - student module - student get detailed document" };
+})
+
+router.post("/register", async (ctx) => {
+    ctx.body = { message: "flow - student module - student registration" };
+    //will call the mailing module
+})
+
+router.post("/login", async (ctx) => {
+    //the actual login will be done inside the auth gateway
+    //this endpoint will retrieve data such as Options, profile informations
+    //credits, etc
+    //if the password should be changed, etc
+    ctx.body = { message: "flow - student module - student login data" };
+})
+
+router.post("/credits", async (ctx) => {
+    ctx.body = { message: "flow - student module - student credits purchase" };
+})
+
+router.post("/options", async (ctx) => {
+    ctx.body = { message: "flow - student module - student add or update options" };
+})
+
+router.put("/password", async (ctx) => {
+    ctx.body = { message: "flow - student module - student password change" };
+})
+
+router.put("/ticket", async (ctx) => {
+    ctx.body = { message: "flow - student module - update student entity with ticket value" };
+})
+
+
 module.exports = router;
