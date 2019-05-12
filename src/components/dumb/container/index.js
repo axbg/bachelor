4070d2feee802fcdbcc8a7a673e551415911df36
Alex Bisag <bisagalexstefan@gmail.com>
2019-05-12 15:52:55 +0300

    Created App Shell.

---
+++ src/components/dumb/container/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import "./index.css";
+
+class Container extends Component {
+
+    render() {
+        return (
+            <div className="grid-container">
+                <div className="app-container">
+                    sal
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Container;
