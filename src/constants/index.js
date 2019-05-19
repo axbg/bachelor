3c258217a719930255010c8554aaa14f6d3aa61e
Alex Bisag <bisagalexstefan@gmail.com>
2019-05-19 21:16:19 +0300

    Fixed BottomNavigationBar - keeping position at refresh.

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,24 +9,28 @@ export const USER_ROLES = {
 
 export const STUDENT_NAVIGATION_OPTIONS = [
     {
+        index: 0,
         title: "Home",
         shortTitle: "Home",
         icon: "icon",
         link: "/student/home"
     },
     {
+        index: 1,
         title: "Profile",
         shortTitle: "Profile",
         icon: "icon",
         link: "/student/profile"
     },
     {
+        index: 2,
         title: "Registration Day",
         shortTitle: "Registration",
         icon: "icon",
         link: "/student/registration-day"
     },
     {
+        index: 3,
         title: "Credits and Faculties",
         shortTitle: "Credits",
         icon: "icon",
