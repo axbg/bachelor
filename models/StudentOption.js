555f5b387964a04611e2e011b3345a97eee4e0e2
Alex Bisag <bisagalexstefan@gmail.com>
2019-05-20 23:30:01 +0300

    Refactored.

--- models/StudentOption.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-    return sequelize.define("student_option", {
-        "study_form": {
-            type: DataTypes.STRING(1),
-            allowNull: false
-        }
-    })
-}
