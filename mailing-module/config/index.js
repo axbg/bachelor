ea4356bffeda2a50457d4c6a7c3163bbe946e916
Alex Bisag <bisagalexstefan@gmail.com>
2019-06-20 23:38:00 +0300

    Updated directory structure.

---
+++ mailing-module/config/index.js
@@ -0,0 +1,9 @@
+const PORT = 8002;
+const MAILGUN_API_KEY = "04b548da6a05cd6f350d8a870022b7c3-52b0ea77-ba8af6bf";
+const MAILGUN_DOMAIN = "sandboxaeb78c2559bf434da1d436fdd4bb3a59.mailgun.org";
+
+module.exports = {
+    PORT,
+    MAILGUN_API_KEY,
+    MAILGUN_DOMAIN
+}
