33935c619b43ecffaf1352db7e415eb208f8eb2f
Alex Bisag <bisagalexstefan@gmail.com>
2019-06-24 12:47:27 +0300

    Added deployment files.

---
+++ deployment_script.sh
@@ -0,0 +1,9 @@
+#install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+#deploy instance
+sudo docker-compose up -d
+echo "Flow instances deployed successfully"
