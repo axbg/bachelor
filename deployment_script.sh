52c275655bdac642978f8689a3094eaa0ac30f6d
Alex Bisag <bisagalexstefan@gmail.com>
2019-06-24 13:01:43 +0300

    Updated deployment_script and docker-compose.

--- deployment_script.sh
+++ deployment_script.sh
@@ -1,9 +1,20 @@
+#update apt
+sudo apt-get update
+
 #install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
 
+#install docker compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+sudo chmod +x /usr/bin/docker-compose
+
 #deploy instance
 sudo docker-compose up -d
+clear
 echo "Flow instances deployed successfully"
