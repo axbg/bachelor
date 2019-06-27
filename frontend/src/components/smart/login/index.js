1b631ec5beeca6f8f4ab95e417e7c2210b86e52c
Alex Bisag <bisagalexstefan@gmail.com>
2019-06-27 13:31:25 +0300

    Finished Admin layout. Implemented Student EnrollmentDay first step. Finished Register and Login Layout.

--- frontend/src/components/smart/login/index.js
+++ frontend/src/components/smart/login/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './index.css';
 import { authenticate } from '../../../reducers/authReducer';
+import { TextField } from '@material-ui/core';
+
 
 class Login extends Component {
     fakeLogin() {
@@ -13,12 +15,31 @@ class Login extends Component {
         return (
             <div className="login-page">
                 <div className="form">
-                    <img src="/step1w.png"  alt="STEP_1"/>
+                    <img src="/logo_transparent.png" width="250" alt="logo" />
+                    <br />
+                    <br />
                     <div className="login-form">
-                        <input type="text" placeholder="username" />
-                        <input type="password" placeholder="password" />
-                        <button onClick={() => { this.fakeLogin() }}>login</button>
-                        <p className="message">Not registered? <a href="/register">Create an account</a></p>
+                        <TextField
+                            fullWidth
+                            label="Email"
+                            onChange={(value) => { this.setState({ username: value.value }) }}
+                            name="email"
+                            variant="outlined"
+                        />
+                        <br/>
+                        <br/>
+                        <TextField
+                            fullWidth
+                            label="Password"
+                            onChange={(value) => { this.setState({ password: value.value }) }}
+                            name="email"
+                            variant="outlined"
+                            type="password"
+                        />
+                        <br/>
+                        <br/>
+                        <button onClick={() => { this.fakeLogin() }}>Logare</button>
+                        <p className="message">Nu te-ai înscris încă? <a href="/register">Crează un cont!</a></p>
                     </div>
                 </div>
             </div>
