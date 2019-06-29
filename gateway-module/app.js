303fe0f73eaf4ef5ac09d0b8205bda4401845ad2
Alex Bisag <bisagalexstefan@gmail.com>
2019-06-29 23:58:04 +0300

    Testing proxy custom HTTP header append.

--- gateway-module/app.js
+++ gateway-module/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const proxy = require('http-proxy-middleware');
 const constants = require('./constants');
 const Student = require('./models/index').Student;
 const User = require('./models/index').User;
@@ -8,25 +7,48 @@ const JWT_SECRET = require('constants')
 
 const app = express();
 
-app.use("/login", async (ctx) => {
+const proxy_options = {
+    onProxyReq(proxyReq, req, res) {
+        proxyReq.setHeader('test', 'da');
+    }
+}
+
+const proxy = require('http-proxy-middleware');
+
+app.get("/", async (req, res) => {
     //search for a student based on email and password
     //if the email contains "ase" consult the User Model
     //else consult the Student model
     //encrypt data resulted from the search in a jwt
     //return the jwt
+    res.status(200).send({ message: "flow - gateway module" });
 });
 
+app.get("/test1", (req, res) => {
+    console.log(req.headers);
+    res.status(200).send({ message: "good" });
+})
+
+app.use("/test", proxy({
+    target: "http://localhost:8010", pathRewrite: { "/test": "/test1" },
+    onProxyReq(proxyReq, req, res) {
+        proxyReq.setHeader('X-CUSTOM', "TESTING");
+    }
+}))
+
+
+
 app.use("/student", proxy({
-    target: constants.BASE_URL + constants.STUDENT_MODULE_PORT, pathRewrite: {
+    target: constants.BASE_URL + ":" + constants.STUDENT_MODULE_PORT, pathRewrite: {
         "/student": ""
     }
 }))
 
 app.use("/mail", proxy({
-    target: constants.BASE_URL + constants.MAILING_MODULE_PORT, pathRewrite: {
+    target: constants.BASE_URL + ":" + constants.MAILING_MODULE_PORT, pathRewrite: {
         "/mail": ""
     }
 }))
 
 app.listen(constants.GATEWAY_MODULE_PORT,
-    () => console.log("Gateway module started on " + constants.BASE_URL + constants.GATEWAY_MODULE_PORT));
+    () => console.log("Gateway module started on " + constants.BASE_URL + ":" + constants.GATEWAY_MODULE_PORT));
