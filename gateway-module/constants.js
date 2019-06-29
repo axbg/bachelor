7ff6dccb166ea81a24b748ebb51322ea6080e655
Alex Bisag <bisagalexstefan@gmail.com>
2019-06-29 23:58:04 +0300

    Testing proxy custom HTTP header append.

--- gateway-module/constants.js
+++ gateway-module/constants.js
@@ -1,7 +1,9 @@
 module.exports = {
     JWT_SECRET: "licenta",
-    BASE_URL:  "http://localhost:",
-    GATEWAY_MODULE_PORT: "8000",
-    STUDENT_MODULE_PORT: "8001",
-    MAILING_MODULE_PORT: "8002"
+    BASE_URL: "http://localhost",
+    STUDENT_BASE_URL: "http://flow-student",
+    MAILING_BASE_URL: "http://flow-mailing",
+    GATEWAY_MODULE_PORT: 8010,
+    STUDENT_MODULE_PORT: 8001,
+    MAILING_MODULE_PORT: 8002
 }
