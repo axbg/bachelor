a5e40fda06f2c5c7e03fafce31dc7aa4a895ac4e
Alex Bisag <bisagalexstefan@gmail.com>
2019-07-12 14:37:54 +0300

    Sync with all modules.

--- gateway-module/services/authentication.js
+++ gateway-module/services/authentication.js
@@ -1,12 +1,14 @@
 module.exports.extractFromJWT = (proxyReq, req, res) => {
     validateType(req.baseUrl, req.user.type, res);
+
     proxyReq.setHeader('X-ID', req.user.id);
-    proxyReq.setHeader('X-EMAIL', req.user.email || req.user.username);
+    proxyReq.setHeader('X-EMAIL', req.user.email || "");
+    proxyReq.setHeader('X-USERNAME', req.user.username || "");
     proxyReq.setHeader('X-TYPE', req.user.type);
 };
 
 const validateType = (baseUrl, type, res) => {
-    if ((type === "STUDENT" && !baseUrl.includes("/student"))) {
+    if ((type === "STUDENT" && (!baseUrl.includes("/student") && !baseUrl.includes('payment')))) {
         return res.status(401).send({ message: "Unauthorized" });
     }
 }
